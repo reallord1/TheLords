f31044428de0ea5f0dc61ff8440b762482468df6
egge-jil <egge.jilc.2022@ksz.edu-zg.ch>
2026-02-16 16:49:13 +0100

    update code.py

--- Code.py
+++ Code.py
@@ -4,7 +4,7 @@ import random
 
 py.init()
 
-#Grund Ding
+# Grund Ding
 win_size = (800, 800)
 screen = py.display.set_mode(win_size)
 py.display.set_caption("Hide and Seek")
@@ -16,8 +16,8 @@ background = py.image.load("Hintergrund.png").convert()
 background = py.transform.scale(background, (800, 800))
 
 # Spieler grösse
-spieler_breite = 120
-spieler_hoehe = 120
+spieler_breite = 90
+spieler_hoehe = 130
 
 # Laufbilder
 WalkRight = [
@@ -42,18 +42,43 @@ WalkLeft = [
     py.image.load("pictures/johannes/run.l.8.png")
 ]
 
-standing = py.image.load("pictures/johannes/stand.l.png")
-standing = py.transform.scale(standing, (spieler_breite, spieler_hoehe))
+# Sprungbilder
+JumpRight = [
+    py.image.load("pictures/johannes/Jump.r.1.png"),
+    py.image.load("pictures/johannes/Jump.r.2.png"),
+    py.image.load("pictures/johannes/Jump.r.3.png"),
+    py.image.load("pictures/johannes/Jump.r.4.png")
+]
+
+JumpLeft = [
+    py.image.load("pictures/johannes/Jump.l.1.png"),
+    py.image.load("pictures/johannes/Jump.l.2.png"),
+    py.image.load("pictures/johannes/Jump.l.3.png"),
+    py.image.load("pictures/johannes/Jump.l.4.png")
+]
+
+stand_left = py.image.load("pictures/johannes/stand.l.png")
+stand_right = py.image.load("pictures/johannes/stand.r.png")
+
+stand_left  = py.transform.scale(stand_left, (spieler_breite, spieler_hoehe))
+stand_right = py.transform.scale(stand_right, (spieler_breite, spieler_hoehe))
 
 sit_right = py.image.load("pictures/johannes/sit.r.png")
 sit_left  = py.image.load("pictures/johannes/sit.l.png")
 
-sit_right = py.transform.scale(sit_right, (spieler_breite, spieler_hoehe))
-sit_left  = py.transform.scale(sit_left, (spieler_breite, spieler_hoehe))
+# Duck-Bilder kleiner
+duck_hoehe = 100
+
+sit_right = py.transform.scale(sit_right, (spieler_breite, duck_hoehe))
+sit_left  = py.transform.scale(sit_left,  (spieler_breite, duck_hoehe))
 
 for i in range(len(WalkRight)):
     WalkRight[i] = py.transform.scale(WalkRight[i], (spieler_breite, spieler_hoehe))
     WalkLeft[i] = py.transform.scale(WalkLeft[i], (spieler_breite, spieler_hoehe))
+
+for i in range(len(JumpRight)):
+    JumpRight[i] = py.transform.scale(JumpRight[i], (spieler_breite, spieler_hoehe))
+    JumpLeft[i]  = py.transform.scale(JumpLeft[i], (spieler_breite, spieler_hoehe))
 
 # Spieler
 class Player:
@@ -71,12 +96,19 @@ class Player:
         self.ducken = False
         self.last_direction = "right"
 
+        self.jump = False
+        self.jumpCount = 0
+        self.start_y = self.y
+
     def move(self):
         keys = py.key.get_pressed()
         moving_left  = keys[py.K_LEFT]
         moving_right = keys[py.K_RIGHT]
-        
-        
+
+        if keys[py.K_UP] and not self.jump:
+            self.jump = True
+            self.jumpCount = 0
+
         self.ducken = keys[py.K_DOWN] and not (moving_left or moving_right)
 
         if moving_left:
@@ -96,12 +128,28 @@ class Player:
             self.right = False
             self.walkCount = 0
 
+        if self.jump:
+            if self.jumpCount < 28:
+                self.y = self.start_y - (28 - self.jumpCount) * 2.4
+            else:
+                self.y = self.start_y
+                self.jump = False
+            self.jumpCount += 1
+
     def draw(self):
+        if self.jump:
+            frame = min(self.jumpCount // 6, 3)
+            if self.last_direction == "right":
+                screen.blit(JumpRight[frame], (self.x, self.y))
+            else:
+                screen.blit(JumpLeft[frame], (self.x, self.y))
+            return
+
         if self.ducken:
             if self.last_direction == "right":
-                screen.blit(sit_right, (self.x, self.y))
+                screen.blit(sit_right, (self.x, self.y + spieler_hoehe - duck_hoehe))
             else:
-                screen.blit(sit_left, (self.x, self.y))
+                screen.blit(sit_left, (self.x, self.y + spieler_hoehe - duck_hoehe))
             return
 
         if self.walkCount + 1 >= 24:
@@ -110,11 +158,16 @@ class Player:
         if self.left:
             screen.blit(WalkLeft[self.walkCount // 3], (self.x, self.y))
             self.walkCount += 1
+
         elif self.right:
             screen.blit(WalkRight[self.walkCount // 3], (self.x, self.y))
             self.walkCount += 1
+
         else:
-            screen.blit(standing, (self.x, self.y))
+            if self.last_direction == "right":
+                screen.blit(stand_right, (self.x, self.y))
+            else:
+                screen.blit(stand_left, (self.x, self.y))
 
 # Hindernisse
 class Hindernis:
@@ -147,6 +200,7 @@ hindernisse = [
     Hindernis("Laptop.png", 220),
     Hindernis("Test.png", 150),
 ]
+
 
 running = True
 while running:
